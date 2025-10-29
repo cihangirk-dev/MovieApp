7b8d37c1adbf453a6361d167d1608f94107fb4ee
cihangirk-dev <cihangirkankaya@gmail.com>
2025-10-29 15:53:39 +0300

    Initial Commit

---
+++ MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by Cihangir Kankaya on 29.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
